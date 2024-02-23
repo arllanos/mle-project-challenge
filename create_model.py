cb20a15cc5224bdab3173cc0093a6973c7b4336d
arllanos <ariel.llanos@gmail.com>
2024-02-23 17:27:37 -0300

    feat: add model evaluation metrics (#2)

--- create_model.py
+++ create_model.py
@@ -5,13 +5,15 @@ from typing import List
 from typing import Tuple
 
 import pandas
+import numpy as np
 from sklearn import model_selection
 from sklearn import neighbors
 from sklearn import pipeline
 from sklearn import preprocessing
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 SALES_PATH = "data/kc_house_data.csv"  # path to CSV with home sale data
-DEMOGRAPHICS_PATH = "data/kc_house_data.csv"  # path to CSV with demographics
+DEMOGRAPHICS_PATH = "data/zipcode_demographics.csv"  # path to CSV with demographics
 # List of columns (subset) that will be taken from home sale data
 SALES_COLUMN_SELECTION = [
     'price', 'bedrooms', 'bathrooms', 'sqft_living', 'sqft_lot', 'floors',
@@ -40,7 +42,7 @@ def load_data(
     data = pandas.read_csv(sales_path,
                            usecols=sales_column_selection,
                            dtype={'zipcode': str})
-    demographics = pandas.read_csv("data/zipcode_demographics.csv",
+    demographics = pandas.read_csv(demographics_path,
                                    dtype={'zipcode': str})
 
     merged_data = data.merge(demographics, how="left",
@@ -56,11 +58,23 @@ def main():
     """Load data, train model, and export artifacts."""
     x, y = load_data(SALES_PATH, DEMOGRAPHICS_PATH, SALES_COLUMN_SELECTION)
     x_train, _x_test, y_train, _y_test = model_selection.train_test_split(
-        x, y, random_state=42)
+        x, y, test_size=0.2, random_state=42)
 
     model = pipeline.make_pipeline(preprocessing.RobustScaler(),
                                    neighbors.KNeighborsRegressor()).fit(
                                        x_train, y_train)
+    
+    y_pred = model.predict(_x_test)
+
+    mae = mean_absolute_error(_y_test, y_pred)
+    mse = mean_squared_error(_y_test, y_pred)
+    rmse = np.sqrt(mse)
+    r2 = r2_score(_y_test, y_pred)
+
+    print(f"Mean Absolute Error.....: {mae:,.2f}")
+    print(f"Mean Squared Error......: {mse:,.2f}")
+    print(f"Root Mean Squared Error.: {rmse:,.2f}")
+    print(f"R-squared Score.........: {r2:.2f}")
 
     output_dir = pathlib.Path(OUTPUT_DIR)
     output_dir.mkdir(exist_ok=True)
